7d26d1c57cf60712798146640aa8e3403d5a676b
Carlos Guzmán <guzmanc@utb.edu.co>
2024-10-11 23:24:01 -0500

    3306

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,6 +1,7 @@
 module.exports = {
-  HOST: "206.189.207.91",
+  HOST: "206.189.207.91",  // Dirección IP del Droplet de MySQL
   USER: "admin",
   PASSWORD: "af2547750c3137ef873ca465910310f82bd1d60721ad1e7e",
-  DB: "testdb"
+  DB: "testdb",
+  PORT: 3306  // Puerto de MySQL (opcional si es el predeterminado)
 };
