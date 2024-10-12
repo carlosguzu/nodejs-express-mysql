7575b92bde05ac01cddc6d66cade63abb431129a
Carlos Guzmán <guzmanc@utb.edu.co>
2024-10-12 10:28:04 -0500

    update ip

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  HOST: "165.227.205.33",  // Dirección IP del Droplet de MySQL
+  HOST: "157.245.83.234",  // Dirección IP del Droplet de MySQL
   USER: "admin",
   PASSWORD: "af2547750c3137ef873ca465910310f82bd1d60721ad1e7e",
   DB: "testdb"
