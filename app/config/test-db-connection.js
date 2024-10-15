9283127507148c0ed07fd110214355f29f2e788f
Carlos Guzmán <guzmanc@utb.edu.co>
2024-10-15 15:24:26 -0500

    test db file added host localhost

---
+++ app/config/test-db-connection.js
@@ -0,0 +1,19 @@
+const mysql = require("mysql2");
+const dbConfig = require("./db.config.js");
+
+const connection = mysql.createConnection({
+  host: dbConfig.HOST,
+  user: dbConfig.USER,
+  password: dbConfig.PASSWORD,
+  database: dbConfig.DB
+});
+
+connection.connect(error => {
+  if (error) {
+    console.error("Error al conectar a la base de datos:", error);
+    process.exit(1);
+  } else {
+    console.log("Conexión exitosa a la base de datos.");
+    connection.end();
+  }
+});
